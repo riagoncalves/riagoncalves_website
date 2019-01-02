ceb32924468cbc311b6c530427016526e4f3169e
riagoncalves <ricardo.goncalves252@gmail.com>
2019-01-02 16:02:31 +0000

    views and routs change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admins
-  
+
+  devise_scope :admin do
+    get 'login', to: 'devise/sessions#new', as: :new_main_admin_session
+    post 'login', to: 'devise/sessions#create', as: :main_admin_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :main_admin_destroy
+  end
+
   authenticated :admin do
     resources :admins
   end
