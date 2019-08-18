bead28b7cc0f6f0079c5ce29f1b09cd49e2e1c95
riagoncalves <ricardo.goncalves252@gmail.com>
2019-08-18 16:33:41 +0100

    admin account as env vars

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin.create(
-  email: 'admin@riagoncalves.com',
-  password: 'admin1'
+  email: ENV["ADMIN_EMAIL"],
+  password: ENV["ADMIN_PASSWORD"]
 )
