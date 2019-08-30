387dada6420bc4561572095f2d73dbf43749a065
riagoncalves <ricardo.goncalves252@gmail.com>
2019-08-30 22:22:00 +0100

    finished styling home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @profile = Profile.instance
+    @skills = Skill.all.order(:created_at)
+    @contacts = Contact.all.order(:created_at)
   end
 end
 
